4c07d11fac8c731e9ba190a33f559c8002740445
takina888 <takinaito@gmail.com>
2026-06-11 10:16:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('rice-app-ver17').then(c=>c.addAll(['./','index.html','manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='rice-app-ver18-pass4158';
+const ASSETS=['./','index.html','manifest.json','sw.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
