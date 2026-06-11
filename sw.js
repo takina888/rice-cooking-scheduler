aa0bd9334c586eebe5c7b9d8536ca9b54b6746de
takina888 <takinaito@gmail.com>
2026-06-11 09:56:10 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('rice-app-ver17').then(c=>c.addAll(['./','index.html','manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
